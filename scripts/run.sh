5c718e53a04c2d29830e7e3961e4cba9cd663643
Nyah Check <check.nyah@gmail.com>
2016-12-10 06:59:25 +0100

    Added launch script for election UI.

---
+++ scripts/run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "puebe binary dir:" "$DIR"
+
+pushd "$DIR" >/dev/null
+
+go run cmd/main.go --gui-dir="${DIR}/src/gui/static/" $@
+
+popd >/dev/null
